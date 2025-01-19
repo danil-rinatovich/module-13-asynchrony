66a3b898a27dac66481671f685d1a8ab49b7cfe1
danil_rinatovich <danil_rinatovich@list.ru>
2025-01-19 23:46:39 +0500

    finished

---
+++ module_13_2.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, executor,types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+
+token_api = ''
+bot = Bot(token=token_api)
+dispatcher = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dispatcher.message_handler(commands=['start'])
+async def start_message(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dispatcher.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ =='__main__':
+    executor.start_polling(dispatcher, skip_updates=True)
